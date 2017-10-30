0f805aca750d1169423fad88930a3f4f72c20931
KekeTSC <kelian.daste31@gmail.com>
2017-10-30 15:31:14 +0100

    Ajout photos gallery appareil

--- app/src/main/java/fr/wcs/wishlist/AddActivity.java
+++ app/src/main/java/fr/wcs/wishlist/AddActivity.java
@@ -1,78 +1,41 @@
 package fr.wcs.wishlist;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class AddActivity extends AppCompatActivity {
 
-    public static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 99;
-    private static final int PICK_IMAGE_REQUEST = 2;
-    private static final int TAKE_IMAGE_REQUEST = 4;
-    private ImageView mImageView;
-    private Uri mFilePath;
-    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    public static String mViaName = "";
-    private ImageButton mFloatingActionButton;
-    private AlertDialog dialog;
-    private ProgressBar mProgressBar;
-    private ImageView mBeMyFirst;
-    private TextView mUploadInfo;
-    private TextView mInfoDialog;
-    private StorageReference mStorageReference;
-    public FirebaseStorage mStorage;
+    private static final int GALLERY_INTENT = 2;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    //recyclerview object
-    private RecyclerView recyclerView;
+    private FirebaseStorage mFirebaseStorage;
+    private ProgressDialog mProgressDialog;
 
-    //adapter object
-    private RecyclerView.Adapter adapter;
-
+    private AlertDialog alert;
 
     @Override
     protected void onCreate (final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        mProgressDialog = new ProgressDialog(this);
 
-        ImageButton imageWish = (ImageButton) findViewById(R.id.imageWish);
+        mFirebaseStorage = FirebaseStorage.getInstance();
+
+        ImageButton imageWish =  findViewById(R.id.imageWish);
         imageWish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,7 +43,7 @@ public class AddActivity extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(AddActivity.this);
 
                 builder.setView(view);
-                AlertDialog alert = builder.create();
+                alert = builder.create();
                 alert.show();
                 ImageButton takeImage = view.findViewById(R.id.takeImage);
                 takeImage.setOnClickListener(new View.OnClickListener() {
@@ -95,158 +58,58 @@ public class AddActivity extends AppCompatActivity {
                     public void onClick(View view) {
                         Intent intent = new Intent(Intent.ACTION_PICK);
                         intent.setType("image/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent, "Selectionner une image"), PICK_IMAGE_REQUEST);
+                        startActivityForResult(intent, GALLERY_INTENT);
 
                     }
                 });
             }
         });
     }
+
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null
-                && data.getData() != null) {
-            mFilePath = data.getData();
-            uploadFromPath(mFilePath);
-        } else if (requestCode == TAKE_IMAGE_REQUEST && resultCode == RESULT_OK) {
-            checkPermission();
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+
+            mProgressDialog.setMessage("Uploading");
+            mProgressDialog.show();
+
+            StorageReference photopath = mFirebaseStorage.getReference("Photos").child(uri.getLastPathSegment());
+
+            photopath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(AddActivity.this, "Succes", Toast.LENGTH_SHORT).show();
+                    alert.dismiss();
+                    mProgressDialog.dismiss();
+                }
+            });
         }
-    }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+                Uri uri = data.getData();
 
+                mProgressDialog.setMessage("Uploading");
+                mProgressDialog.show();
 
-    String mCurrentPhotoPath;
-    Uri mCurrentPhotoUri;
+                StorageReference photopath = mFirebaseStorage.getReference("Photos").child(uri.getLastPathSegment());
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = AddActivity.this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
+                photopath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(AddActivity.this, "Succes", Toast.LENGTH_SHORT).show();
+                        alert.dismiss();
+                        mProgressDialog.dismiss();
+                    }
+                });
+            }
+        }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(AddActivity.this.getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                ;
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                mCurrentPhotoUri = FileProvider.getUriForFile(AddActivity.this,
-                        "fr.wcs.wishlist.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mCurrentPhotoUri);
-                startActivityForResult(takePictureIntent, TAKE_IMAGE_REQUEST);
-            }
-        }
-    }
-
-    public void checkPermission() {
-
-        uploadFromPath(mCurrentPhotoUri);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_WRITE_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //La personne a accepté les permissions
-                    checkPermission();
-                }
-            }
-        }
-    }
-
-    public void uploadFromPath(final Uri path) {
-        if (path != null) {
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            Bitmap bitmap = null;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(AddActivity.this.getContentResolver(),mCurrentPhotoUri);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
-            byte[] data = baos.toByteArray();
-
-            final StorageReference photoRef = mStorageReference.child("image/" + mViaName.replace(" ", "_") + "/" + path.getLastPathSegment());
-            photoRef.putFile(path)
-                    //viaRef.putBytes(data)
-
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            ImageButton takeImage = findViewById(R.id.takeImage);
-                            ImageButton selectImage = findViewById(R.id.selectImage);
-                            mProgressBar.setVisibility(View.GONE);
-                            mUploadInfo.setVisibility(View.GONE);
-                            takeImage.setVisibility(View.GONE);
-                            selectImage.setVisibility(View.GONE);
-                            mInfoDialog.setVisibility(View.GONE);
-
-                            Toast.makeText(AddActivity.this, "Image envoyée ", Toast.LENGTH_LONG).show();
-                            dialog.dismiss();
-                            DatabaseReference imageRef = mDatabase.getReference("photos");
-                            PhotoModel newPhoto = new PhotoModel("example",photoRef.toString());
-                            imageRef.push().setValue(newPhoto);
-
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-
-                            Toast.makeText(AddActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
-
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-
-                            ImageButton takeImage = findViewById(R.id.takeImage);
-                            ImageButton selectImage = findViewById(R.id.selectImage);
-
-                            float progress = 100f * ((float)taskSnapshot.getBytesTransferred() / (float)taskSnapshot.getTotalByteCount());
-                            System.out.println("Upload is " + progress + "% done");
-                            int currentProgress = (int) progress;
-                            mProgressBar.setProgress(currentProgress);
-                            mProgressBar.setVisibility(View.VISIBLE);
-                            mUploadInfo.setText("Envoi en cours :" +" " + String.valueOf(currentProgress) +"%");
-                            mUploadInfo.setVisibility(View.VISIBLE);
-                            takeImage.setVisibility(View.GONE);
-                            selectImage.setVisibility(View.GONE);
-                            mInfoDialog.setVisibility(View.GONE);
-                            mImageView.setVisibility(View.GONE);
-
-                        }
-                    });
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 }
