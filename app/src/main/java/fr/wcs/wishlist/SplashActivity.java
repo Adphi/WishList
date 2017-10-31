8e6b261dcb62ba0a8bf0d889b014531aa11be28c
KekeTSC <kelian.daste31@gmail.com>
2017-10-31 10:57:42 +0100

    Splash

---
+++ app/src/main/java/fr/wcs/wishlist/SplashActivity.java
@@ -0,0 +1,30 @@
+package fr.wcs.wishlist;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 4500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        //splash screen code
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
