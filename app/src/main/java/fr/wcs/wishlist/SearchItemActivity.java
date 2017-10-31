1ebe510c3217962844f8e1185ff0d3635ff573f9
Philippe-Adrien Nousse <philippe.adrien.nousse@gmail.com>
2017-10-31 03:57:27 +0100

    Request First Step

--- app/src/main/java/fr/wcs/wishlist/SearchItemActivity.java
+++ app/src/main/java/fr/wcs/wishlist/SearchItemActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.SearchView;
 
-import com.loopj.android.http.AsyncHttpClient;
-
+import java.io.IOException;
 import java.util.ArrayList;
 
 import fr.wcs.wishlist.Controller.ItemAdapter;
+import fr.wcs.wishlist.Models.CDiscount.RequestBuilder;
 import fr.wcs.wishlist.Models.Item;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SearchItemActivity extends AppCompatActivity {
 
@@ -22,13 +29,40 @@ public class SearchItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_item);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewGift);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewSearch);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         mItemAdapter = new ItemAdapter(this, mItems, ItemAdapter.State.GIFT);
         recyclerView.setAdapter(mItemAdapter);
 
-        AsyncHttpClient client = new AsyncHttpClient();
+        SearchView searchView = (SearchView) findViewById(R.id.searchViewItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/octet-stream");
+        RequestBody body = RequestBody.create(mediaType, new RequestBuilder("Tablette", RequestBuilder.Sort
+                .RELEVANCE, 5, 0).build());
+        Request request = new Request.Builder()
+                .url("https://api.cdiscount.com/OpenApi/json/Search")
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            Log.d("HELPER", "onCreate: " + response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
